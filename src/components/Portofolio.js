160038a26c9349de09d198e014e3cee389c6ef91
krisnafirdaus <krisnafirdaus@gmail.com>
2022-04-26 01:10:54 +0800

    fix href

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -2,111 +2,111 @@ import React from "react";
 
 const Portofolio = () => {
   return (
-    <section class="has-img-bg">
-      <div class="container">
-        <h6 class="section-subtitle mt-5 text-center">Great Food</h6>
-        <h3 class="section-title mb-6 text-center">Main Menu</h3>
-        <div class="card bg-light">
-          <div class="card-body px-4 pb-4 text-center">
-            <div class="row text-left">
-              <div class="col-md-6 my-4">
+    <section className="has-img-bg">
+      <div className="container">
+        <h6 className="section-subtitle mt-5 text-center">Great Food</h6>
+        <h3 className="section-title mb-6 text-center">Main Menu</h3>
+        <div className="card bg-light">
+          <div className="card-body px-4 pb-4 text-center">
+            <div className="row text-left">
+              <div className="col-md-6 my-4">
                 <a
-                  href="#"
-                  class="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
+                  href="/#"
+                  className="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
                 >
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
+                  <div className="d-flex">
+                    <div className="flex-grow-1">
                       Dolorem Exmaiores
-                      <p class="mt-1 mb-0">
+                      <p className="mt-1 mb-0">
                         Numquam dolor dolores molestiae maiores quidem.
                       </p>
                     </div>
-                    <h6 class="float-right text-primary">$12</h6>
+                    <h6 className="float-right text-primary">$12</h6>
                   </div>
                 </a>
               </div>
-              <div class="col-md-6 my-4">
+              <div className="col-md-6 my-4">
                 <a
-                  href="#"
-                  class="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
+                  href="/#"
+                  className="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
                 >
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
+                  <div className="d-flex">
+                    <div className="flex-grow-1">
                       Aperiam incidunt dicta
-                      <p class="mt-1 mb-0">
+                      <p className="mt-1 mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit.
                       </p>
                     </div>
-                    <h6 class="float-right text-primary">$23</h6>
+                    <h6 className="float-right text-primary">$23</h6>
                   </div>
                 </a>
               </div>
-              <div class="col-md-6 my-4">
+              <div className="col-md-6 my-4">
                 <a
-                  href="#"
-                  class="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
+                  href="/#"
+                  className="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
                 >
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
+                  <div className="d-flex">
+                    <div className="flex-grow-1">
                       Animi repellat labore
-                      <p class="mt-1 mb-0">
+                      <p className="mt-1 mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit.
                       </p>
                     </div>
-                    <h6 class="float-right text-primary">$32</h6>
+                    <h6 className="float-right text-primary">$32</h6>
                   </div>
                 </a>
               </div>
-              <div class="col-md-6 my-4">
+              <div className="col-md-6 my-4">
                 <a
-                  href="#"
-                  class="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
+                  href="/#"
+                  className="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
                 >
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
+                  <div className="d-flex">
+                    <div className="flex-grow-1">
                       Quo ipsum similique
-                      <p class="mt-2 mb-0">
+                      <p className="mt-2 mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit.
                       </p>
                     </div>
-                    <h6 class="float-right text-primary">$17</h6>
+                    <h6 className="float-right text-primary">$17</h6>
                   </div>
                 </a>
               </div>
-              <div class="col-md-6 my-4">
+              <div className="col-md-6 my-4">
                 <a
-                  href="#"
-                  class="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
+                  href="/#"
+                  className="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
                 >
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
+                  <div className="d-flex">
+                    <div className="flex-grow-1">
                       Est nam tempore
-                      <p class="mt-2 mb-0">
+                      <p className="mt-2 mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit.
                       </p>
                     </div>
-                    <h6 class="float-right text-primary">$21</h6>
+                    <h6 className="float-right text-primary">$21</h6>
                   </div>
                 </a>
               </div>
-              <div class="col-md-6 my-4">
+              <div className="col-md-6 my-4">
                 <a
-                  href="#"
-                  class="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
+                  href="/#"
+                  className="pb-3 mx-3 d-block text-dark text-decoration-none border border-left-0 border-top-0 border-right-0"
                 >
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
+                  <div className="d-flex">
+                    <div className="flex-grow-1">
                       Nesciunt ab veniam
-                      <p class="mt-2 mb-0">
+                      <p className="mt-2 mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit.
                       </p>
                     </div>
-                    <h6 class="float-right text-primary">$10</h6>
+                    <h6 className="float-right text-primary">$10</h6>
                   </div>
                 </a>
               </div>
